b109bf6379dc499def841fe2ab0acdfe8e592b4c
unknown <jjjk0605@naver.com>
2025-08-09 02:43:54 +0900

    chore: shadcn sonner 설치

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
     <Analytics />
   </StrictMode>
 );
